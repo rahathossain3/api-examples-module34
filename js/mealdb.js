d30729ef528fec72274a8885dc5b037d5efc7804
rahathossain3 <rahathossain210@gmail.com>
2022-03-01 00:55:47 +0600

    show a item

--- js/mealdb.js
+++ js/mealdb.js
@@ -15,11 +15,11 @@ const displaySearchResult = meals => {
     const searchResult = document.getElementById('search-result');      // ger parent node
     //start function and get single elemtnt of meals
     meals.forEach(meal => {
-        console.log(meal);
+        // console.log(meal);
         const div = document.createElement('div');              //careat chald node
-        div.classList.add('col');                    //add class in create element
+        div.classList.add('col');                    //add class in create element  & add a onclick function
         div.innerHTML = `
-        <div class="card h-100">
+        <div onclick= "loadMealDetail(${meal.idMeal})" class="card h-100">
                 <img src=" ${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
@@ -29,5 +29,28 @@ const displaySearchResult = meals => {
         `;
         searchResult.appendChild(div);          //append chield         in      parent NOde
     });
+}
+// for onclick dynamic
+const loadMealDetail = mealId => {
+    // console.log(mealId);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetail(data.meals[0]))
+}
 
+const displayMealDetail = meal => {
+    console.log(meal);
+    const mealDetail = document.getElementById('meal-details');
+    const div = document.createElement('div')
+    div.classList.add('card');
+    div.innerHTML = `
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text"> ${meal.strInstructions.slice(0, 150)}</p>
+                <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+            </div>
+    `;
+    mealDetail.appendChild(div);
 }
